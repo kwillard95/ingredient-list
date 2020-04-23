ab8b7e1e8277154d764f955ef7ceebd310b6763d
kwillard95 <kwillard95@gmail.com>
2020-04-22 22:58:20 -0700

    Destructures useState return values

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  const inputState = useState({ title: "", amount: "" });
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -20,10 +20,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="text"
               id="title"
-              value={inputState[0].title}
+              value={inputState.title}
               onChange={(e) => {
                 const newTitle = e.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   title: newTitle,
                   amount: prevInputState.amount,
                 }));
@@ -35,10 +35,10 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState[0].amount}
+              value={inputState.amount}
               onChange={(e) => {
                 const newAmount = e.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   amount: newAmount,
                   title: prevInputState.title,
                 }));
